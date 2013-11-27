7a15008ab015e9a6552f3963a7ed5e17763c595f
kshramt <thisisdummy@example.com>
2013-11-27 21:08:51 +0900

    Add `make_fixed_format_parser`

--- kshramt.py
+++ kshramt.py
@@ -49,6 +49,35 @@ def list_2d(n_row, n_column):
             in range(n_row)]
 
 
+def make_fixed_format_parser(fields):
+    """
+    fields: [{'name': 'density', 'converter': int,   'length': 3},
+             {'name': 'opacity', 'converter': float, 'length': 7}]
+    """
+    lower = 0
+    _fields = []
+    for field in fields:
+        assert field['length'] >= 1
+        _field = {k: v for k, v in field.items()}
+        upper = lower + field['length']
+        _field['lower'] = lower
+        _field['upper'] = upper
+        lower = upper
+        _fields.append(_field)
+
+    def fixed_format_parser(s):
+        assert len(s) >= upper
+        ret = {}
+        for field in _fields:
+            name = field['name']
+            f = field['converter']
+            l = field['lower']
+            u = field['upper']
+            ret[name] = f(s[l:u])
+        return ret
+    return fixed_format_parser
+
+
 class TestAction(argparse.Action):
     def __init__(self,
                  option_strings,
@@ -67,7 +96,7 @@ class TestAction(argparse.Action):
         parser.exit()
 
 
-class _Tester(unittest.TestCase):
+class Tester(unittest.TestCase):
     def test_list_2d(self):
         self.assertEqual(list_2d(2, 3),
                          [[None, None, None],
@@ -81,22 +110,29 @@ class _Tester(unittest.TestCase):
         self.assertEqual(tuple(sorted(flatten((1, 2, (3, [4, set([5, 6]), 7 ], [8, 9]))))),
                          tuple(sorted((1, 2, 3, 4, 5, 6, 7, 8, 9))))
 
-
-def _parse_arguments(args=sys.argv[1:]):
-    parser = argparse.ArgumentParser(description='Read a simple Makefile and write a dot file')
-    parser.add_argument('--test',
-                        action=TestAction,
-                        help='run tests')
-    parser.add_argument('--version',
-                        action='version',
-                        version='%(prog)s {version}'.format(version=__version__))
-
-    return parser.parse_args(args)
-
-
-def main(args=sys.argv[1:]):
-    _parse_arguments(args)
+    def test_make_fixed_format_parser(self):
+        with self.assertRaises(AssertionError):
+            make_fixed_format_parser([{'name': 'a',
+                                       'converter': int,
+                                       'length': 0}])
+        fixed_format_parser\
+            = make_fixed_format_parser([{'name': 'a',
+                                         'converter': int,
+                                         'length': 3},
+                                        {'name': 'b',
+                                         'converter': lambda x: -int(x),
+                                         'length': 7}])
+        self.assertEqual(fixed_format_parser(' 325      '),
+                         {'a': 32, 'b': -5})
+        self.assertEqual(fixed_format_parser(' 325      \n'),
+                         {'a': 32, 'b': -5})
+        self.assertEqual(fixed_format_parser(' 32  5    '),
+                         {'a': 32, 'b': -5})
+        self.assertEqual(fixed_format_parser('32   5    abc'),
+                         {'a': 32, 'b': -5})
+        with self.assertRaises(AssertionError):
+            fixed_format_parser('123456789')
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    unittest.main()
