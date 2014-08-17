80673c2aadde05d8e8573a6e290a6c7a4b245fc7
Low Kian Seong <kianseong@gmail.com>
2014-08-17 21:21:24 +0800

    Update setup.py
    Adding metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ import kshramt
 
 setup(name='kshramt',
       version=kshramt.__version__,
+      description='kshramt',
+      url='https://github.com/kshramt/kshramt.py',
       py_modules=['kshramt'])
